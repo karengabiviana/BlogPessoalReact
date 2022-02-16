9ea33d6fd0361ebeaa39d53513fac99e5350cc96
karengabiviana <karengabrieli.viana@outloo.com>
2022-02-16 10:49:47 -0300

    Adicionando primeiro componente

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Grid, Box, Typography, Button } from '@material-ui/core';
+import './Home.css';
+
+function Home() {
+    return (
+        <>
+            <h1 className='titulo'>Home</h1>
+            <img src='https://arteeartistas.com.br/wp-content/uploads/2018/09/Noite-Estrelada.-Vincent-van-Gogh.jpg' alt='Parte do Quadro "Noite Estrelada" de Van Gogh' className='img'></img>
+        </>
+    );
+}
+
+export default Home;
