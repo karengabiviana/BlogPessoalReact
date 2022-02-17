5b27e7ddf3d36f9aacd26f363b37d5a4a9366341
karengabiviana <karengabrieli.viana@outloo.com>
2022-02-16 22:13:09 -0300

    Adicionando Navbar, Home e Footer - React

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import Home from './paginas/home/Home'
+import Navbar from './components/Statics/navbar/Navbar';
+import Home from './paginas/home/Home';
+import Footer from './components/Statics/footer/Footer';
+import { Grid } from '@material-ui/core';
 import './App.css';
 
 function App() {
   return (
   <>
-    <Home/>
+    <Navbar />
+    <Home  />
+    <Footer/>
+
   </>
   );
 }
