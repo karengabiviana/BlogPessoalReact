e56da5e08e8ca1dc6bffa07f480a240427a38d60
karengabiviana <karengabrieli.viana@outloo.com>
2022-03-11 08:56:07 -0300

    Atualizando Footer para o deploy

--- src/components/Statics/footer/Footer.tsx
+++ src/components/Statics/footer/Footer.tsx
@@ -25,17 +25,17 @@ function Footer()
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
                             <Typography variant="h5" align="center" gutterBottom className="texto">Siga-nos nas redes sociais </Typography>
                         </Box>
-                        <Box display="flex" alignItems="center" justifyContent="center">
+                        {/* <Box display="flex" alignItems="center" justifyContent="center">
                             <a href="https://www.facebook.com/generationbrasil" target="_blank">
                                 <FacebookIcon className="redes" />
                             </a>
-                            {/* <a href="https://www.instagram.com/generationbrasil/" target="_blank">
+                             <a href="https://www.instagram.com/generationbrasil/" target="_blank">
                                 <InstagramIcon className="redes" /> */}
-                            {/* </a> */}
+                            {/* </a> 
                             <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
                                 <LinkedInIcon className="redes" />
                             </a>
-                        </Box>
+                        </Box> */}
                     </Box>
                     <Box className="box2">
                         <Box paddingTop={1}> 
